7af7f155eea84b8a21d7f015295be873d9e63350
edson-magombo <bed-com-33-18@cc.ac.mw>
2021-03-08 04:38:37 -0800

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Avatar} from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime"
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutLineIcon from "@material-ui/icons/HelpOutline";
+import "./Header.css";
+function Header(){
+    return(
+        <div className= "header">
+            <div className= "header__left">
+            <Avatar className="header__avatar" alt= "Eddie Gomboz"></Avatar>
+            <AccessTimeIcon />
+            </div>
+            <div className= "header__search">
+                <SearchIcon />
+                <input placeholder = "search chatroom" />
+            </div>
+            <div className= "header__left">
+                <HelpOutLineIcon />
+            </div>
+        </div>
+    )
+}
+export default Header;
