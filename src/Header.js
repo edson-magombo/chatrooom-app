06797de3ee07807b69cd065dc7dc73cb66cb0c0c
edson-magombo <bed-com-33-18@cc.ac.mw>
2021-03-21 09:41:14 -0700

    its tough here

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Avatar} from "@material-ui/core";
-import AccessTimeIcon from "@material-ui/icons/AccessTime"
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutLineIcon from "@material-ui/icons/HelpOutline";
 import "./Header.css";
