6b1366e255585dc82473c3dd738d03512f222fe5
edson-magombo <bed-com-33-18@cc.ac.mw>
2021-03-22 23:11:43 -0700

    i can code now kkk i just wanted to check my exam result to continue with this kkk coding again  ... mood come on lets do this kkk im goona do this -power-

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,12 +3,12 @@ import "./Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
 import CreateIcon from "@material-ui/icons/Create";
 import InsertCommentIcon from "@material-ui/icons/InsertComment"
-import SidebarOptsion from "./SidebarOption";
+import SidebarOption from "./SidebarOption";
 import InboxIcon from "@material-ui/icons/Inbox";
 import DraftsIcon from "@material-ui/icons/Drafts"
 import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import PeopleAltIcon from "@material-ui/icons/PeopleAlt"
-import AppsIcon from "@material-ui/icons/Appss"
+import AppsIcon from "@material-ui/icons/Apps"
 import FileCopyIcon from "@material-ui/icons/FileCopy"
 import ExpandLessIcon from "@material-ui/icons/ExpandLess"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
